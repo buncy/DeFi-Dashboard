de0ce2dd9bed1e8e2083fa327a14d29c099e6e10
buncy <karthik138105@gmail.com>
2021-03-06 14:52:53 +0530

    Updates

---
+++ precache-manifest.0b022db6c37fff5c4e5ae0f59f99cd52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a4c2d9e99dfd9f9002f3efcf966ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52fd34e01e954059b2e8",
+    "url": "/static/css/main.89562762.chunk.css"
+  },
+  {
+    "revision": "bdee24a2d78f21535b67",
+    "url": "/static/js/2.1a5b62f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a5b62f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fd34e01e954059b2e8",
+    "url": "/static/js/main.8c6e7446.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  }
+]);
